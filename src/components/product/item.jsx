c0312d31b86a95c268641a81260fe18d05234d57
0hu <dladudgn123@naver.com>
2022-05-19 16:25:28 +0900

    ProductList style적용

--- src/components/product/item.jsx
+++ src/components/product/item.jsx
@@ -1,12 +1,28 @@
-const ProductItem = ({ category, description, image, price, rating, title}) => (
-  <li>
-    <p>{category}</p>
-    <p>{title}</p>
-    <p>{description}</p>
-    <img src={image} />
-    <span>${price}</span>
-    <span>{rating.rate}</span>
-  </li>
+import styled from '@emotion/styled'
+import {Link} from "react-router-dom";
+
+let ProductList = styled.li({
+  border: '1px solid #000000',
+  padding: '10px',
+  img:{
+    width: '100%',
+    height: '300px',
+    objectFit: 'contain'
+  }
+})
+
+
+
+
+
+const ProductItem = ({ image, rating, title, id}) => (
+  <Link to={`/products/${id}`}>
+    <ProductList>
+      <p>{title}</p>
+      <img src={image} />
+      <span>{rating.rate}</span>
+    </ProductList>
+  </Link>
 )
 
 export default ProductItem;
--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,7 +1,14 @@
 import {useQuery} from "react-query";
 import {fetcher, QueryKeys} from "../../queryClient";
 import ProductItem from "../../components/product/item";
+import styled from '@emotion/styled';
 
+let Products = styled.ul({
+  listStyle: 'none',
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gap: '0.63em',
+})
 
 const ProductList = () => {
   const { data } = useQuery(QueryKeys.PRODUCTS, () =>
@@ -14,11 +21,11 @@ const ProductList = () => {
 
   return (
     <div>
-      <ul>
+      <Products>
         {data ?.map(product => (
           <ProductItem {...product} key={product.id} />
         ))}
-      </ul>
+      </Products>
     </div>
   );
 };
