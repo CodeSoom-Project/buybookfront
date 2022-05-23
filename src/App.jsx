51aa7ee4456f9488e098d7693ebc82acde0e25d3
0hu <dladudgn123@naver.com>
2022-05-23 09:50:04 +0900

    Page path 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { QueryClientProvider } from 'react-query'
-import {useRoutes} from "react-router-dom";
+import {QueryClientProvider} from 'react-query'
+import {Route, Routes} from "react-router-dom";
 import {getClient} from "./queryClient";
-import {routes} from "./routes";
-import { ReactQueryDevtools } from 'react-query/devtools'
+import {ReactQueryDevtools} from 'react-query/devtools'
+import MainPage from "./pages";
+import ProductList from "./pages/products";
+import ProductDetailPage from "./pages/products/[id]";
+import Cart from "./pages/cart";
+import NotFoundPage from "./pages/NotFoundPage";
+import Layout from "./pages/_layout";
+import SearchResultContainer from "./pages/searchResultContainer";
 
 
 const App = () => {
-  const elem = useRoutes(routes)
+  // const elem = useRoutes(routes)
   const queryClient = getClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      {elem}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="/products" element={<ProductList/>}/>
+          <Route path="/products/:id" element={<ProductDetailPage/>}/>
+          <Route path="/search/:word" element={<SearchResultContainer/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="/*" element={<NotFoundPage/>}/>
+        </Route>
+      </Routes>
+      <ReactQueryDevtools initialIsOpen={false}/>
     </QueryClientProvider>
   )
 }
--- src/pages/_layout.jsx
+++ src/pages/_layout.jsx
@@ -1,11 +1,13 @@
-import { Suspense } from "react";
-import { Outlet } from 'react-router-dom';
+import {Suspense} from "react";
+import {Outlet} from 'react-router-dom';
+import Header from "../components/Header";
 
 const Layout = () => {
   return (
     <div>
+      <Header/>
       <Suspense fallback={'loading...'}>
-        <Outlet />
+        <Outlet/>
       </Suspense>
     </div>
   );
